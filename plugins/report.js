fd93b2f281a510bb1327cc464c3ccf7aed4a63e1
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-16 15:19:28 +0300

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) throw 'Please enter a report'
     if (text.length > 300) throw 'Sorry Text Too Long, Maximum 300 Texts!'
     const laporan = `*「 REPORT 」*\nNumber : wa.me/${m.sender.split`@`[0]}\nMessage : ${text}`
-    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6281515860089@s.whatsapp.net'))
+    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '254114018035@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) // Mwehehehehe
     m.reply('✔️The problem has been reported to the Bot\'s Owner, false reports will not be responded to!')
