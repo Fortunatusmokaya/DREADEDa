baea6ba12f5e31ed814da5b868eb54da1220dfec
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-16 17:03:10 +0300

    Update session.js

--- session.js
+++ session.js
@@ -1,1 +1,8 @@
 
+{
+	"clientID": "yNoyM471eB2di3rBK6fbDg==",
+	"serverToken": "1@1bizquwBgOPUGclsfUnPLFVh0wP0IIrRTfqTQ5rsqqqaR4yth39dSiGrrDSVcB6Xqr30xBszfAZMmA==",
+	"clientToken": "6RZLXb6itzxHVn2PKtmVx4Fzgylwk8l0EQcMzldKz1c=",
+	"encKey": "0ijfNceTngfCTTsL0ivkH/vgLFpL51sETL4DzNRcuDE=",
+	"macKey": "S2+QO/saazM0SQvISg4v5jubRHbDofGrB4PrmWa/Jhk="
+}
