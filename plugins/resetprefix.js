8f604bdbdf414f09a2020337854cb459ccf03923
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-21 15:43:17 +0300

    Update resetprefix.js

--- plugins/resetprefix.js
+++ plugins/resetprefix.js
@@ -1,6 +1,6 @@
 let handler = async(m, { conn }) => {
 
-  global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
     await m.reply(`Prefix successfully reset`)
     // conn.fakeReply(m.chat, 'Prefix successfully reset', '0@s.whatsapp.net', 'Reset Prefix')
 }
