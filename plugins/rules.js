3d9066b9f3597507707c916bca89ddfb6b7bba89
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-21 16:02:00 +0300

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,15 +1,19 @@
 let handler = async m => m.reply(`
 💎 *DREADED BOT RULES*
 
-💎 _Do not call bot, if you do then you'll get automatically blocked by bot._
+🤖 _Bot *MUST BE ADMIN* of groups added._
+
+💎 _Its completely unnecessary to call a bot, rather you will be blocked._
     
 💎 _Do not spam bot with commands, if bot is not responding then it means either bot is off or there's internet issue at owner's end._
     
-💎 _Do not abuse/disrespect bot and its owner,its completely unneccesary, rather appreciate whatever._
+💎 _Bot or its owner are not responsible for what the bot sends in terms of media._
     
-💎 _If you see any bug/error in bot then report it to owner with the command !bug/report <problem>._
+💎 _Well, the bot has not been installed with ffmpeg package and probably may not send some image media!_
     
-💎 _If you want this bot in your group then contact owner by typing !owner/creator._
+💎 _Usage of bot in ways that promote racism, slavery, pollution etc will lead to bot exiting the group or ban._
+
+🌀 _Bot ain't perfect and probably has some bugs, be patient and report them to owner!_
     
 🎇 _Enjoy whatever the bot offers and break above rules, Issa free world_
    
