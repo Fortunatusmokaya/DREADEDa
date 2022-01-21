1b39e9a6f15568d3db41407c679b105ac460d70d
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-21 15:45:28 +0300

    Update setprefixbot.js

--- plugins/setprefixbot.js
+++ plugins/setprefixbot.js
@@ -1,6 +1,6 @@
 let handler = async(m, { conn, text }) => {
   if (!text) throw `No Prefix detected...`
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
   await m.reply(`Prefix has been changed to *${text}*`)
     // conn.fakeReply(m.chat, 'Prefix has been changed to *${text}*', '0@s.whatsapp.net', 'Set Prefix Bot')
 }
