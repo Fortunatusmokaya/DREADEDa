3fa3ab08e0c3b424ecfea1829d0077edcf7803f1
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-24 22:44:54 +0300

    Create Ariel.js

---
+++ plugins/Ariel.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, text }) => {
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+
+  conn.reply(m.chat, `
+That is my *owner*>:( Leave him Alone..🌚>:(
+`.trim(), m)
+    let mentionedJid = [m.sender]
+}
+handler.customPrefix = /@254114018035/i
+handler.command = new RegExp
+
+module.exports = handler
