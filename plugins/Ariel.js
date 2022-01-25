daa50ef2279afec38fed9cd1bd76d07ad5d728e0
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-25 15:57:35 +0300

    Update Ariel.js

--- plugins/Ariel.js
+++ plugins/Ariel.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-That is my *owner*>:( Leave him Alone..🌚>:(
+That is my *OWNER*❤ >:( Leave him Alone...;)🌚>:(
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
