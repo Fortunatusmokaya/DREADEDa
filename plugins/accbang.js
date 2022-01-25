8e8f3f2978b2f5d8b8ff1b5bd28fb18394fb8e01
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-25 16:51:09 +0300

    Update accbang.js

--- plugins/accbang.js
+++ plugins/accbang.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, isAdmin }) => {
   if (isAdmin) throw 'I have already done that! You are admin!'
   await conn.groupMakeAdmin(m.chat, [m.sender])
 }
-handler.command = /^do it.$/i
+handler.command = /^done.$/i
 handler.owner = true
 handler.botAdmin = true
 module.exports = handler
