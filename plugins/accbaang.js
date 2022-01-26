22a30e837f52ccb5f67fdc66ab0064cb1fe57afc
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-26 13:25:10 +0300

    Delete accbaang.js

--- plugins/accbaang.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, participants }) => {
-  if (m.fromMe) throw 'no'
-  if (isAdmin) throw '*You are No longer Admin Already!*'
-  await conn.groupDemoteAdmin(m.chat, [m.sender])
-}
-handler.command = /^undo.$/i
-handler.owner = true
-handler.botAdmin = true
-module.exports = handler
