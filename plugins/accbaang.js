f649124d4713344ae69140acaa0481b38f90ce2e
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-26 12:45:59 +0300

    Update accbaang.js

--- plugins/accbaang.js
+++ plugins/accbaang.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin }) => {
   if (m.fromMe) throw 'no'
   if (isAdmin) throw '*Fortunne* You are No longer Admin Already!'
-  await conn.groupMakeAdmin(m.chat, [m.sender])
+  await conn.groupDemoteAdmin(m.chat, [m.sender])
 }
 handler.command = /^undo.$/i
 handler.owner = true
