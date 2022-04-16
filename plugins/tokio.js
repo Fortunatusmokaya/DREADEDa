35799357339b7e5b72713f53c3ac5537300bc910
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-04-16 12:10:16 +0300

    Update tokio.js

--- plugins/tokio.js
+++ plugins/tokio.js
@@ -1,5 +1,5 @@
 let handler = async m => m.reply(`
-💎 *Dreaded Bot*\n\n🔗 *Info :* Very easy and fast to deploy termux-heroku WhatsApp bot written in Js, Modified Tokio-wabot. Credits to Jay,Nurutumo and Fortunne
+💎 *Dreaded Bot*\n\n🔗 *Info :* Very easy and fast to deploy termux-heroku WhatsApp bot written in Js, Modified Tokio-wabot. Credits to Ajay and Nurutumo 
 `.trim()) // repository
 handler.help = ['dreaded']
 handler.tags = ['info']
