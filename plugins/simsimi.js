c13073cd3e0673a1c62edf6f8aa10288794ef7e2
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-04-16 22:32:55 +0300

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "en" }, ''))
+  let res = await fetch(global.API('http://api.brainshop.ai/get?bid=163086&key=t78dOnZP5GQL5olm&uid=[uid]&msg=[msg]', '/v2/', { text: encodeURIComponent(text), lc: "en" }, ''))
   let json = await res.json()
   if (json.success) m.reply(json.success)
   else throw json
