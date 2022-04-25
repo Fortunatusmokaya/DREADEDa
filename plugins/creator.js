9af2c8cc81352ab459642f49b7d60565957424d3
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-04-26 00:43:06 +0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '254114018035', 'Fortunne mokaya', m)
-  this.sendContact(m.chat, '254114018035', 'Fortunne/mokaya', m)
+  this.sendContact(m.chat, '254114018035', 'Mokaya 🐦', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
