1820983efb7ab814c23b3bc9271e2d79006a8e72
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-09 01:50:41 +0300

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,23 +1,26 @@
 let handler = async m => m.reply(`
-💎 *DREADED BOT RULES*
+💎 *Baymax Bot Rules©*
 
 🤖 _Bot *MUST BE ADMIN* of groups added._
 
-💎 _Its completely unnecessary to call a bot, rather you will be blocked._
+🧧 _Its completely unnecessary to call a bot, rather you will be blocked.♦️_
     
-💎 _Do not spam bot with commands, if bot is not responding then it means either bot is off or there's internet issue at owner's end._
+🧧 _Do not spam bot with commands, if bot is not responding then it means either bot is off or there's internet issue at owner's end.♦️_
     
-💎 _Bot or its owner are not responsible for what the bot sends in terms of media._
+🧧 _Bot or its owner are not responsible for what the bot sends in terms of media.♦️_
     
-💎 _Well, the bot has not been installed with ffmpeg package and probably may not send some image media!_
+🧧 _Well, the bot has not been installed with ffmpeg package and probably may not send some image media!♦️_
     
-💎 _Usage of bot in ways that promote racism, slavery, pollution etc will lead to bot exiting the group or ban._
+🧧 _Usage of bot in ways that promote racism, slavery, pollution etc will lead to bot exiting the group or ban.♦️_
 
-🌀 _Bot ain't perfect and probably has some bugs, be patient and report them to owner!_
+🧧 _Bot ain't perfect and probably has some bugs, be patient and report them to owner!♦️_
     
-🎇 _Enjoy whatever the bot offers and break above rules, Issa free world_
-   
- 💖 _By Fortunne._
+🧧 _Enjoy whatever the bot offers and break above rules, Its a free world♦️_
+
+🎎 _Go n sleep if you dont like my bot🏮_
+ 
+ 💉 *𝕵𝔲𝔦𝔠𝔢 𝖂𝕽𝕷𝕯 𝑺𝑼𝑷𝑹𝑬𝑴𝑨𝑪𝒀ꫂ᭄⃟🔥*
+
   `.trim()) // Tambah sendiri kalo mau
 handler.help = ['rules']
 handler.tags = ['info']
