128af910eb01ed70ab22988ba682c6d48ee9268a
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-09 02:13:15 +0300

    Update botstat.js

--- plugins/botstat.js
+++ plugins/botstat.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn }) => {
     let uptime = clockString(_uptime)
 
     m.reply(`
-┌─〔 Dreaded Status 〕
+┌─〔 Baymax Status 〕
 ├ *${groups.length}* Group
 ├ *${chats.length - groups.length}* Private Chat
 ├ *${Object.keys(global.db.data.users).length}* Users
