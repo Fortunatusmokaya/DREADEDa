e903611f26878fcb8032a27629a0387cad3ef832
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-09 02:04:12 +0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '254114018035', 'Fortunne mokaya', m)
-  this.sendContact(m.chat, '254114018035', 'Mokaya 🐦', m)
+  // F this.sendContact(m.chat, '263780699988', '𝕵𝔲𝔦𝔠𝔢 𝖂𝕽𝕷𝕯 𝑺𝑼𝑷𝑹𝑬𝑴𝑨𝑪𝒀ꫂ᭄⃟🔥', m)
+  this.sendContact(m.chat, '263780699988', '𝕵𝔲𝔦𝔠𝔢 𝖂𝕽𝕷𝕯 𝑺𝑼𝑷𝑹𝑬𝑴𝑨𝑪𝒀ꫂ᭄⃟🔥', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
