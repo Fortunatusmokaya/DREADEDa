c3741a4dc45804f361686c48425608014cda29f3
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-22 01:37:39 +0300

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -28,7 +28,7 @@ let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
 *Title:* ${title}
 *Filesize:* ${filesizeF}
 *Source:* ${vid.url}
- ${isLimit ? 'Used ': ''}
+  Sending Audio 🚦
 `.trim(), m)
 let _thumb = {}
 try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
