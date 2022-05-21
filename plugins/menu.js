91df15b84fb9cec8a469666d0a124b4f852aa45d
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-22 00:27:48 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,72 +1,105 @@
-let fs = require('fs')
-let path = require('path')
-let fetch = require('node-fetch')
-let levelling = require('../lib/levelling')
-const thumb = fs.readFileSync('./src/IMG-20220509-WA0023.jpg')
+import { promises } from 'fs'
+import { join } from 'path'
+import { xpRange } from '../lib/levelling.js'
 let tags = {
   'main': 'MAIN',
-  'info': 'INFO',
-  'game': 'GAME',
-  'xp': 'EXP & LIMIT',
-  'sticker': 'STICKER',
-  'admin': 'ADMIN',
-  'group': 'GROUP',
-  'premium': 'PREMIUM',
-  'anime': 'ANIME',
-  'internet': 'INTERNET',
-  'nulis': 'LOGO & WRITING',
-  'downloader': 'DOWNLOADER',
-  'tools': 'TOOLS',
-  'fun': 'FUN',
-  'audio': 'AUDIO',
-  'maker': 'MAKER',
-  'videomaker': 'VIDEOMAKER',
-  'database': 'DATABASE',
-  'exp': 'REGISTER',
-  'vote': 'VOTING',
-  'absen': 'ABSENT',
-  'jadibot': 'GET BOT',
-  'anonymous': 'ANONYMOUS CHAT',
-  'owner': 'OWNER',
-  'host': 'HOST',
-  'advanced': 'ADVANCED',
+  'game': '𝗚𝗔𝗠𝗘',
+  'rpg': '𝗥𝗣𝗚 𝗚𝗔𝗠𝗘𝗦',
+  'xp': '𝗫𝗣 & 𝗟𝗜𝗠𝗜𝗧',
+  'sticker': '𝗦𝗧𝗜𝗖𝗞𝗘𝗥',
+  'kerang': '𝗞𝗘𝗥𝗔𝗡𝗚',
+  'quotes': '𝗤𝗢𝗨𝗧𝗘𝗦',
+  'group': '𝗚𝗥𝗢𝗨𝗣',
+  'internet': '𝗜𝗡𝗧𝗘𝗥𝗡𝗘𝗧',
+  'anonymous': '𝗔𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦 𝗖𝗛𝗔𝗧',
+  'nulis': '𝗟𝗢𝗚𝗢 𝗠𝗔𝗞𝗘𝗥',
+  'anime': '𝗔𝗡𝗜𝗠𝗘',
+  'nsfw': '𝗡𝗦𝗙',
+  'downloader': '𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥',
+  'tools': '𝗧𝗢𝗢𝗟𝗦',
+  'fun': '𝗙𝗨𝗡',
+  'quran': '𝗔𝗜',
+  'owner': '𝗢𝗪𝗡𝗘𝗥',
+  'info': '𝗜𝗡𝗙𝗢',
 }
 const defaultMenu = {
   before: `
+  ╭─━━━━━━━━━━━━━━━━━
+  ┃✾┈[ *─ׅ─⃝Notes📮Side─ׅ─⃝*  ]┈✾
+  ┖━━━━━━━━━━━━━━━━━━
+  Owner Bot not responsible for any inconvenience caused by bot
 
-👋🏿 Hello %name, how can i -help you?
+ Bot Does not store any users data so don't fear about security of your account
 
-〽️ Prefix: *%p*
-📅 Date: *%week, %date*
-💠 Owner: 𝕵𝔲𝔦𝔠𝔢 𝖂𝕽𝕷𝕯 𝑺𝑼𝑷𝑹𝑬𝑴𝑨𝑪𝒀ꫂ᭄⃟🔥
+ Read FAQ or rules 
+ If you find any error in bot please type %powner to contact owner
 
-👇🏻 Tap read more for all my commands below, Do not spam!
 
+ENJOY %me .inc Bots🚀
+
+1 | _Do not call bot, if you do then you'll get automatically blocked by bot._
+    
+2 | _Do not spam bot with commands, if bot is not responding then it means either bot is off or there's internet issue at owner's end._
+    
+3 | _Do not abuse/disrespect bot and its owner._
+    
+4 | _If you see any bug/error in bot then report it to owner with the command !bug/report <problem>._
+    
+5 | _If you want this bot in your group then contact owner by typing !owner/creator._
+    
+6 | _Enjoy the bot and have fun._
+
+7 | _There's 3 Types of Users
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+
+╭━━✾━━━━━━━━━━━━━✾━
+┃      ❰ ✾ 𝗨𝗦𝗘𝗥 ✾ ❱
+┟━━✾━━━━━━━━━━━━━✾━
+┃  
+┃ *NAME* : *%name*
+┃  𝗟𝗜𝗠𝗜𝗧 : *%limit Limit*
+┃  𝗥𝗢𝗟𝗘 : *%role*
+┃  𝗟𝗘𝗩𝗘𝗟 : *%level (%exp / %maxexp)*
+┃  𝗧𝗢𝗧𝗔𝗟 𝗫𝗣 : %totalexp ✨
+┃ 
+┃  𝗗𝗔𝗧𝗘: %date*
+┃  𝗧𝗜𝗠𝗘: *%time*
+┃
+┃  𝗨𝗣𝗧𝗜𝗠𝗘: *%uptime (%muptime)*
+┃  𝗗𝗔𝗧𝗔𝗕𝗔𝗦𝗘: %rtotalreg of %totalreg
+┗━━━━━━━━━━━━━━━━⦂
 %readmore`.trimStart(),
-  header: '        *━━❰･%category･❱━━*',
-  body: ' 📌 %cmd %islimit %isPremium',
-  footer: ' ',
-  after: `📌 *Baymax bot by 𝕵𝔲𝔦𝔠𝔢 𝖂𝕽𝕷𝕯 𝑺𝑼𝑷𝑹𝑬𝑴𝑨𝑪𝒀ꫂ᭄⃟🔥, nice day, cool* 
+  after: `
+  *⊹  ࣪. ִ . ࣪ ⊹  ࣪. ִ . ࣪ ⊹   ࣪. ִ . ࣪ ⊹  ࣪. ִ . ࣪ ⊹ 
+  ׄ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ
+ ░⃟ 
+ *٫٫ 🪧★ ࣪ ،💐🄰🅃🅃🄴🄽🅃🄸🄾🄽 𝆺̸𝅥💐 ›
+ ░⃟🛹 𓂅 ָ࣪ ۰BayMax Bot is not online 24hrs
+ *٫٫ 🪧★ ࣪ ،💐Rights Reserved𝆺̸𝅥💐
+  ׄ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ ▬ׄ ▭ׅ
+  *⊹  ࣪. ִ . ࣪ ⊹  ࣪. ִ . ࣪ ⊹   ࣪. ִ . ࣪ ⊹  ࣪. ִ . ࣪ ⊹ 
+  LICENCI SNI
 `,
 }
-let handler = async (m, { conn, usedPrefix: _p }) => {
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
-    let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
     let { exp, limit, level, role } = global.db.data.users[m.sender]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let name = conn.getName(m.sender)
+    let { min, xp, max } = xpRange(level, global.multiplier)
+    let name = await conn.getName(m.sender)
     let d = new Date(new Date + 3600000)
-    let locale = 'en'
+    let locale = 'id'
     // d.getTimeZoneOffset()
     // Offset -420 is 18.00
     // Offset    0 is  0.00
     // Offset  420 is  7.00
     let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let week = d.toLocaleDateString(locale, { weekday: 'long', timeZone: 'Africa/Harare' })
     let date = d.toLocaleDateString(locale, {
       day: 'numeric',
       month: 'long',
-      year: 'numeric'
+      year: 'numeric',
+      timeZone: 'Africa/Harare'
     })
     let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
       day: 'numeric',
@@ -76,7 +109,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     let time = d.toLocaleTimeString(locale, {
       hour: 'numeric',
       minute: 'numeric',
-      second: 'numeric'
+      second: 'numeric',
+      timeZone: 'Africa/Harare'
     })
     let _uptime = process.uptime() * 1000
     let _muptime
@@ -107,66 +141,48 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
           if (!(tag in tags) && tag) tags[tag] = tag
     conn.menu = conn.menu ? conn.menu : {}
     let before = conn.menu.before || defaultMenu.before
-    let header = conn.menu.header || defaultMenu.header
-    let body = conn.menu.body || defaultMenu.body
-    let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
     let _text = [
       before,
-      ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
-          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-            return  menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
-                .trim()
-            }).join('\n')
-          }),
-          footer
-        ].join('\n')
-      }),
       after
     ].join('\n')
-    text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
     let replace = {
       '%': '%',
       p: _p, uptime, muptime,
-      me: conn.user.name,
-      npmname: package.name,
-      npmdesc: package.description,
-      version: package.version,
+      me: conn.getName(conn.user.jid),
+      npmname: _package.name,
+      npmdesc: _package.description,
+      version: _package.version,
       exp: exp - min,
       maxexp: xp,
       totalexp: exp,
       xp4levelup: max - exp,
-      github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
+      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
       level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.send2ButtonImg(m.chat, thumb,  `📌 *I\'m ${conn.user.name}*`, text.trim(), 'CREATOR', '-owner', 'RULES', '-rules', m)
+    const pp = await promises.readFile(join('./src/avatar_contact.png'))
+    let at1 = ['https://i.pinimg.com/236x/b3/e3/38/b3e338aa349e723cb72b2c6c745e559f.jpg','https://i.pinimg.com/564x/18/f9/d3/18f9d3209f3f5abef5c48046eca0d746.jpg','https://i.pinimg.com/236x/bf/fb/61/bffb6110db24a414cef5eef1df129d7d.jpg','https://i.pinimg.com/236x/9c/14/79/9c1479a17ccba302e334944752d7b6a6.jpg','https://i.pinimg.com/236x/ca/a8/ba/caa8ba4a4629a61b897091b0c8c09226.jpg','https://i.pinimg.com/236x/60/a9/fd/60a9fd07c30f0ebdfbb04635f428fe8e.jpg']
+    let ats1 =  at1[Math.floor(Math.random() * at1.length)]
+    conn.sendHydrated(m.chat, text.trim(), author, ats1, '', 'WEBSITE', '62895383810600', 'CREATOR',[
+      ['OWNER', '/creator'],
+      ['SPEED', '/speed'],
+      ['RULES', '/rules']
+    ], m, { asLocation: 1 })
   } catch (e) {
-    conn.reply(m.chat, 'Sorry, the menu is in error', m)
+    conn.reply(m.chat, 'My friend, menu  error', m)
     throw e
   }
 }
-handler.help = ['help/menu/?']
+handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
 handler.command = /^(menu|help|\?)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 handler.exp = 3
 
-module.exports = handler
+export default handler
 
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
